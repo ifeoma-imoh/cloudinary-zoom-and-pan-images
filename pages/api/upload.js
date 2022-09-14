e24d5f1ca7398ecfbf6b82f92a42f55dc0b6b553
ifeoma-imoh <imoh_ifeoma@yahoo.co.uk>
2022-09-14 13:57:19 +0100

    Added custom attributes

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   try {
     await cloudinary.uploader.upload(req.body.image, (error, result) => {
       const response = cloudinary.video(`${result.public_id}`, {
-        effect: "zoompan:mode_ztl;maxzoom_2.4;du_8",
+        effect: "zoompan:from_(zoom_1;x_300;y_100);to_(zoom_4;x_950;y_800)",
         resource_type: "image",
       });
       res.status(200).json(response);
